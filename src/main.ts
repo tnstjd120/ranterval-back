2819d02fc35c22f5fae3053cd8fdfd94425752a9
moonmoo0403 <moonmoo0403@gmail.com>
2024-09-28 00:08:12 +0900

    chore: swagger모듈, validationPipe 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api/v1');
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.useGlobalPipes(new ValidationPipe({
+    forbidNonWhitelisted : true,
+    transform: true
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('ranterval.API')
+    .setDescription('ranterval-back')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, document, {
+    swaggerOptions: { defaultModelsExpandDepth: -1 }
+  });
+
   await app.listen(3000);
 }
 bootstrap();
